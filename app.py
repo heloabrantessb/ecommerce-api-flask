889a73edb45065e08cab39b53e4219e7ed9c62af
heloabrantessb <heloisa.abrantesdev@gmail.com>
2025-10-26 14:23:28 -0300

    chore: add python-dotenv

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_login import UserMixin, LoginManager, login_user
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce-flask-API.db'
 
 login_manager = LoginManager()
