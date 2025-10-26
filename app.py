731ae5d41f886874336078f075af3e73875debd9
heloabrantessb <heloisa.abrantesdev@gmail.com>
2025-10-26 12:54:29 -0300

    chore: add Flask-CORS initial config

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce-flask-API.db'
 
 db = SQLAlchemy(app)
+CORS(app)
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
