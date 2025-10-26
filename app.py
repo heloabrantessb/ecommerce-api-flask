4743df1598f45eefcb2ba946ed21a5a979e76912
heloabrantessb <heloisa.abrantesdev@gmail.com>
2025-10-25 21:00:43 -0300

    feat: add product creation method

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce-flask-API.db'
 
 db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+@app.route('/api/products/add', methods=['POST'])
+def add_product():
+    data = request.json
+    if 'name'in data and 'price' in data:
+        product = Product(name=data['name'], price=data['price'], description=data.get('description', ""))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message": "Product added successfully"}), 200
+    return jsonify({"message": "Invalid product data"}), 400
+
+@app.route('/api/products/delete/<int: product_id>')
 
 @app.route('/')
 def index():
