1ed4764f340c534ffedfe9614633ecf01ef8b65b
heloabrantessb <heloisa.abrantesdev@gmail.com>
2025-10-26 21:19:54 -0300

    feat: add checkout method

--- app.py
+++ app.py
@@ -164,7 +164,16 @@ def view_cart():
         })
     return jsonify(cart_list), 200
 
+@app.route('/api/cart/checkout', methods=['POST'])
+@login_required
+def checkout():
+    user = User.query.get(int(current_user.id))
+    cart_items = user.cart
 
+    for cart_item in cart_items:
+        db.session.delete(cart_item)
+    db.session.commit()
+    return jsonify({"message": "Checkout successfully. Cart has been clear"})
 
 
 @app.route('/')
