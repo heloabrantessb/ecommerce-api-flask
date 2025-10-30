fabc98570f57cf51ef75d6ae09f612118dc42eda
heloabrantessb <heloisa.abrantesdev@gmail.com>
2025-10-30 00:06:14 -0300

    refactor: separar models do app.py para src/models

--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 import os
 from dotenv import load_dotenv
 
@@ -12,27 +11,9 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce-flask-API.db'
 
 login_manager = LoginManager()
-db = SQLAlchemy(app)
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 CORS(app)
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
-    cart = db.relationship('CartItem', backref='user', lazy=True)
-
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    description = db.Column(db.Text, nullable=True)
-
-class CartItem(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
 
 @login_manager.user_loader
 def load_user(user_id):
---
+++ src/models/product.py
@@ -0,0 +1,7 @@
+from src.database import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
